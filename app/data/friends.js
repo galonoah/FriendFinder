f9ba34c920f089d1b6854c76c9e11bb54d92d4d6
Elmer C. Galo <elmer.galo@outlook.com>
2019-12-06 02:21:13 -0500

    Add users data list

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+
+var usersData = [
+    {
+        name: "Sophie French",
+        photo: "https://live.staticflickr.com/65535/49176798831_19b8d53fe6_w.jpg",
+        scores: [2, 5, 3, 5, 1, 2, 2, 5, 4, 4]
+    },
+    {
+        name: "Eduard Franz",
+        photo: "https://live.staticflickr.com/65535/49176309713_b696bf74d2_w.jpg",
+        scores: [3, 5, 2, 5, 5, 3, 3, 4, 3, 2]
+    },
+    {
+        name: "Janae Randolph",
+        photo: "https://live.staticflickr.com/65535/49176799006_d745b587ea_w.jpg",
+        scores: [5, 1, 1, 1, 2, 3, 2, 3, 3, 4]
+    },
+    {
+        name: "Renee Sims",
+        photo: "https://live.staticflickr.com/65535/49177013562_fae8e0938e_w.jpg",
+        scores: [5, 2, 3, 3, 1, 3, 1, 3, 4, 1]
+    },
+    {
+        name: "Judy Sewell",
+        photo: "https://live.staticflickr.com/65535/49176309448_d46a6329b6_w.jpg",
+        scores: [3, 1, 2, 3, 1, 3, 1, 5, 1, 5]
+    },
+    {
+        name: "Jakoby Roman",
+        photo: "https://live.staticflickr.com/65535/49176799101_1d342962aa_w.jpg",
+        scores: [1, 5, 1, 4, 4, 3, 1, 4, 4, 5]
+    },
+    {
+        name: "Amilia Luna",
+        photo: "https://live.staticflickr.com/65535/49176309763_3e94a16c7a_w.jpg",
+        scores: [3, 3, 3, 3, 4, 5, 2, 2, 3, 2]
+    },
+    {
+        name: "Inara Britt",
+        photo: "https://live.staticflickr.com/65535/49176799211_216e6b01a2_w.jpg",
+        scores: [3, 1, 4, 1, 4, 2, 1, 4, 2, 1]
+    }
+];
+
+module.exports = usersData;

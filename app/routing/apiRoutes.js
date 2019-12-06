1763dd70ce874086b640400aa457fe5fe086d73e
Elmer C. Galo <elmer.galo@outlook.com>
2019-12-06 03:11:21 -0500

    Handle survey results and send it to post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,12 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(usersData);
     });
+
+    app.post("/api/friends", function(req, res){
+        console.log(req.body);
+        
+    });
 }
+
+
+
